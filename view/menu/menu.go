c4f689d8be9765e5dccf461f8c273a422c0c7b5e
flybywind@foxmail.com <flybywind@foxmail.com>
2022-05-23 21:54:52 +0800

    open pdf

--- view/menu/menu.go
+++ view/menu/menu.go
@@ -1,24 +1,44 @@
 package menu
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 
 	"flybywind.me/fyne/config"
+	"flybywind.me/fyne/view/pdfview"
 )
 
-func NewHomeMenu() fyne.CanvasObject {
+func NewHomeMenu(w fyne.Window) fyne.CanvasObject {
 	ln := config.GetLanguage()
-	openMenu := widget.NewButton(config.MenuLangMap[ln][config.MenuOpenTxt], tapOpen)
+	openMenu := widget.NewButton(config.MenuLangMap[ln][config.MenuOpenTxt], func() {
+		tapOpen(w)
+	})
 	shareMenu := widget.NewButton(config.MenuLangMap[ln][config.MenuShareTxt], tapShare)
 	commentMenu := widget.NewButton(config.MenuLangMap[ln][config.MenuCommentTxt], tapComment)
 	return container.NewHBox(openMenu, shareMenu, commentMenu)
 }
 
-func tapOpen() {
+func tapOpen(win fyne.Window) {
 	// todo
-
+	fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+		if err != nil {
+			dialog.ShowError(err, win)
+			return
+		}
+		if reader == nil {
+			log.Println("Cancelled")
+			return
+		}
+		log.Println("open pdf at path:", reader.URI().Path())
+		pdfview.OpenPDF(reader.URI().Path(), win)
+	}, win)
+	fd.SetFilter(storage.NewExtensionFileFilter([]string{".pdf"}))
+	fd.Show()
 }
 func tapShare() {
 	// todo
--- view/pdfview/view.go
+++ view/pdfview/view.go
@@ -1,23 +1,23 @@
 package pdfview
 
 import (
-	"image/color"
+	"fmt"
 	"path"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 
 	"rsc.io/pdf"
-
-	"flybywind.me/fyne/config"
 )
 
 type pdfMeta struct {
-	reader   *pdf.Reader
-	position float32
-	canvas   fyne.CanvasObject
+	pages          []pdf.Page
+	loadingPage    int
+	pageNum        int
+	scrollPosition float32
+	content        *container.Scroll
 }
 
 var (
@@ -27,8 +27,6 @@ var (
 )
 
 func NewPdfViewer() fyne.CanvasObject {
-	rect := canvas.NewRectangle(color.RGBA{255, 0, 0, 255})
-	rect.Resize(fyne.NewSize(config.WindSize.Width/2., config.WindSize.Height/2.))
 	tabs = container.NewAppTabs()
 
 	tabs.OnSelected = func(_ *container.TabItem) {
@@ -38,20 +36,50 @@ func NewPdfViewer() fyne.CanvasObject {
 }
 
 func OpenPDF(fullpath string, parent fyne.Window) error {
-	pdf, err := pdf.Open(fullpath)
+	pdfReader, err := pdf.Open(fullpath)
 	if err != nil {
 		dialog.NewError(err, parent)
 		return err
 	}
+	pageNum := pdfReader.NumPage()
+	pages := make([]pdf.Page, pageNum)
+	for i := 0; i < pageNum; i++ {
+		pages[i] = pdfReader.Page(i)
+	}
 	pdfList = append(pdfList, &pdfMeta{
-		reader:   pdf,
-		position: 0,
-		canvas:   container.NewWithoutLayout(),
+		pages:          pages,
+		loadingPage:    0,
+		pageNum:        pageNum,
+		scrollPosition: 0.0,
+		content:        container.NewScroll(container.NewWithoutLayout()),
 	})
 	curPdf = pdfList[len(pdfList)-1]
 	_, fileName := path.Split(fullpath)
-	tabs.Items = append(tabs.Items, container.NewTabItem(fileName, curPdf.canvas))
+	tabs.Items = append(tabs.Items, container.NewTabItem(fileName, curPdf.content))
 	tabs.SelectIndex(len(tabs.Items) - 1)
 
+	curPdf.renderPage()
 	return nil
+}
+
+func (pdf *pdfMeta) renderPage() {
+	// try to read out some content:
+	contentPdf := curPdf.pages[curPdf.loadingPage].Content()
+	textList := contentPdf.Text
+	content, ok := pdf.content.Content.(*fyne.Container)
+	if !ok {
+		fyne.LogError("pdf content type error", fmt.Errorf("convert error"))
+	}
+	for _, text := range textList {
+		t := widget.NewLabel(text.S)
+		t.Move(fyne.NewPos(float32(text.X), float32(text.Y)))
+		content.Add(t)
+	}
+	pdf.nextPage()
+}
+
+func (pdf *pdfMeta) nextPage() {
+	if pdf.loadingPage < pdf.pageNum-1 {
+		pdf.loadingPage += 1
+	}
 }
